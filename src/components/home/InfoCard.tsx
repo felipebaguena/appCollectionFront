c26335a9cb64eb17681c151d8aae78ec6b1cd6b8
felipebaguena <felipebaguena@gmail.com>
2024-11-07 17:19:27 +0100

    feat: new style in InfoCard

--- src/components/home/InfoCard.tsx
+++ src/components/home/InfoCard.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '../ui/Button';
 import Link from 'next/link';
 
 const CardContainer = styled.div`
-  display: flex;
-  margin: 1rem 1rem;
+  position: relative;
+  width: 100%;
+  height: 400px;
+  margin: 1rem 0;
   overflow: hidden;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
 `;
 
 const ImageSection = styled.div<{ imageUrl: string }>`
-  flex: 1.2;
-  min-height: 300px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-image: url('${props => props.imageUrl}');
   background-size: cover;
-  background-position: center;
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      to left,
+      rgba(0, 0, 0, 0.8) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
 `;
 
 const ContentSection = styled.div`
-  flex: 0.8;
-  padding: 0 2rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  padding: 2rem;
+  color: white;
+  z-index: 1;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -31,14 +52,37 @@ const ContentSection = styled.div`
 
 const Title = styled.h2`
   font-size: 24px;
-  color: #1a1a1a;
+  color: white;
   margin-bottom: 10px;
 `;
 
 const Description = styled.p`
   font-size: 16px;
-  color: #333333;
+  color: rgba(255, 255, 255, 0.9);
   margin-bottom: 20px;
+`;
+
+const InfoLabel = styled.div`
+  background-color: var(--app-yellow);
+  color: var(--dark-grey);
+  padding: 1rem;
+  text-align: center;
+  width: 100%;
+  cursor: pointer;
+  font-weight: bold;
+  transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  transform: scale(1);
+
+  &:hover {
+    background-color: var(--app-yellow-focus);
+    color: black;
+    transform: scale(1.05);
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  width: 100%;
 `;
 
 interface InfoCardProps {
@@ -56,14 +100,11 @@ const InfoCardComponent: React.FC<InfoCardProps> = ({ imageUrl, title, descripti
           <Title>{title}</Title>
           <Description>{description}</Description>
         </div>
-        <Link href="/collection" passHref>
-          <Button
-            type="button"
-            $variant={'dark'}
-          >
+        <StyledLink href="/collection">
+          <InfoLabel>
             Más información
-          </Button>
-        </Link>
+          </InfoLabel>
+        </StyledLink>
       </ContentSection>
     </CardContainer>
   );
